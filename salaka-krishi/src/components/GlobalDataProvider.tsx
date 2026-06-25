db8dbb8d27f29df222361160253fe103474dd802
subashtamang2 <lamasubaah2@gmail.com>
2026-06-25 13:03:02 +0545

    fix: dynamic SEO title and meta description using Helmet

--- salaka-krishi/src/components/GlobalDataProvider.tsx
+++ salaka-krishi/src/components/GlobalDataProvider.tsx
@@ -1,32 +1,66 @@
+
 import { getSiteInfo } from "@src/api/site-info";
 import { type DataWrapper, type SiteInfo } from "@src/schema/schema";
 import { useSiteInfo } from "@src/store/useSiteInfo";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
 
-export default function GlobalDataProvider({ children }: { children: React.ReactNode }) {
+export default function GlobalDataProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    console.log("GlobalDataProvider rendered");
     const setSiteInfo = useSiteInfo((state) => state.setSiteInfo);
 
     const { data } = useQuery<DataWrapper<SiteInfo>>({
-        queryKey: ['site-info'],
+        queryKey: ["site-info"],
         queryFn: async () => {
             const res = await getSiteInfo();
             return res.data;
         },
-        staleTime: 1000 * 60 * 5, // 5 minutes
+        staleTime: 1000 * 60 * 5,
         refetchOnWindowFocus: true,
         refetchOnReconnect: true,
         refetchOnMount: true,
-    })
+    });
 
     const siteInfoData = data?.data;
 
     useEffect(() => {
         if (siteInfoData) {
-            console.log("DEBUG: Global Sync - Fetched Site Info:", siteInfoData);
+            console.log(
+                "DEBUG: Global Sync - Fetched Site Info:",
+                siteInfoData
+            );
             setSiteInfo(siteInfoData);
         }
     }, [siteInfoData, setSiteInfo]);
 
-    return <>{children}</>;
+    return (
+        <>
+            {siteInfoData && (
+                <Helmet>
+                    <title>{siteInfoData.name}</title>
+
+                    <meta
+                        name="description"
+                        content={siteInfoData.description || ""}
+                    />
+
+                    <meta
+                        name="keywords"
+                        content={
+                            Array.isArray(siteInfoData.keywords)
+                                ? siteInfoData.keywords.join(", ")
+                                : ""
+                        }
+                    />
+                </Helmet>
+            )}
+
+            {children}
+        </>
+    );
 }
